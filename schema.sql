30bd4d6c4ebabeb749a77316f82a4ae7343bd581
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-02-06 12:53:32 -0500

    add

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
 -- Drops the todolist if it exists currently --
-DROP DATABASE IF EXISTS todolist;
+DROP DATABASE IF EXISTS qp9em4lr77btyvqz;
 -- Creates the "todolist" database --
-CREATE DATABASE todolist;
+CREATE DATABASE qp9em4lr77btyvqz;
